1ddee587344c9f3bf0c4000e247148de7f3c6387
Kisckk <isackisch@gmail.com>
2025-03-12 18:37:04 -0300

    Add files via upload

---
+++ calculadora-com-funçoes/script.js
@@ -0,0 +1,19 @@
+function somar(a, b){
+    return a + b
+}
+console.log(somar(3, 60))
+
+function subtrair(a, b){
+    return a - b
+}
+console.log(subtrair(30, 6))
+
+function multiplicar(a, b){
+    return a * b
+}
+console.log(multiplicar(10, 5))
+
+function dividir(a, b){
+    return a / b
+}
+console.log(dividir(6, 2))
